b37395940e92255d143d350ec019462f18343a2f
Mustafa <111cetinkaya111@gmail.com>
2023-07-07 18:15:42 +0100

    Test1 has been uploaded

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        System.out.println(" !!! There are still two lessons that i should watch in order to catch the others !!!! ");
+
+
+    }
+}
